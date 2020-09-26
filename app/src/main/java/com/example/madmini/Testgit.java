00dd1ae0c1db7433fc68f52b14e5b896e1319d4e
Nipun Pesala <nipunpesala1996@gmail.com>
2020-09-26 09:51:03 +0530

    name test

---
+++ app/src/main/java/com/example/madmini/Testgit.java
@@ -0,0 +1,14 @@
+package com.example.madmini;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Testgit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testgit);
+    }
+}
---
+++ app/src/main/java/com/example/madmini/testGit2.java
@@ -0,0 +1,14 @@
+package com.example.madmini;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class testGit2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_git2);
+    }
+}
